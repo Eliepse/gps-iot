5e19f0ff1f5b6dd0df4349eba7b904013c2801d5
Élie Meignan <contact@eliepse.fr>
2022-03-28 23:05:32 +0200

    Add fake GPS data on dev

--- src/services/GPSTrackingService.js
+++ src/services/GPSTrackingService.js
@@ -1,6 +1,7 @@
 import {Service} from 'src/services/Service';
 import SerialPort from 'serialport';
 import {TrackerManager} from 'src/tracker/TrackerManager';
+import {GPSState} from 'src/tracker/GPSState';
 
 export class GPSTrackingService extends Service {
 	/** @type {SerialPort} */
@@ -14,7 +15,25 @@ export class GPSTrackingService extends Service {
 	}
 
 	boot() {
+		// Fake manager on dev env
 		if (process.env.APP_ENV === "dev") {
+			const listeners = [];
+			//noinspection JSValidateTypes
+			this._manager = {addListener: (listener) => listeners.push(listener)};
+
+			function fakeUpdate() {
+				const state = new GPSState({
+					lat: 48.81 + (Math.random() * .002),
+					lon: 2.30 + (Math.random() * .002),
+					time: new Date(),
+					satsActive: Array(Math.round(Math.random() * 4)).fill({}),
+					satsVisible: Array(Math.round(Math.random() * 12)).fill({}),
+				});
+				listeners.forEach((listener) => listener.updateState(state));
+				setTimeout(fakeUpdate, Math.round(1500 + Math.random() * 1000));
+			}
+
+			setTimeout(fakeUpdate, 3000);
 			return Promise.resolve();
 		}
 
